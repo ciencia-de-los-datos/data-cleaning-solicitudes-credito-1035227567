76193fa982df8ef0a99afdb0e7191d8be23d35c9
Paola Andrea Gutiérrez <pagutierrezl@unal.edu.co>
2023-04-16 08:22:31 -0500

    Desarrollo
    Desarrollo solicitudes de crédito

--- pregunta.py
+++ pregunta.py
@@ -1,20 +1,16 @@
 """
 Limpieza de datos usando Pandas
 -----------------------------------------------------------------------------------------
-
 Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizada 
 correctamente. Tenga en cuenta datos faltantes y duplicados.
-
 """
 import pandas as pd
 
-
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    data = pd.read_csv("solicitudes_credito.csv", sep=";")
 
-    #
-    # Inserte su código aquí
-    #
+    data.dropna(inplace = True)
+    data.drop_duplicates(inplace = True)
 
     return df
