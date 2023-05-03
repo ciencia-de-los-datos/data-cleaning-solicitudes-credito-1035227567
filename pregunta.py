7ae639162331a179fc8d783e20e460656523d7c7
Paola Andrea Gutiérrez <pagutierrezl@unal.edu.co>
2023-05-03 17:52:25 -0500

    Solución
    Realizada

--- pregunta.py
+++ pregunta.py
@@ -10,7 +10,31 @@ def clean_data():
 
     data = pd.read_csv("solicitudes_credito.csv", sep=";")
 
+    data.rename(columns = {'Unnamed: 0': 'index'}, inplace = True)
+    data.set_index('index', inplace = True)
+
+    data.sexo = data.sexo.str.lower()
+    data.sexo = data.sexo.astype('category')
+
+    data.tipo_de_emprendimiento = data.tipo_de_emprendimiento.str.lower()
+    data.tipo_de_emprendimiento = data.tipo_de_emprendimiento.astype('category')
+
+    data.idea_negocio = data.idea_negocio.str.lower().str.strip('_').str.strip('-').str.strip().str.replace('_',' ').str.replace('-',' ')
+    data.idea_negocio = data.idea_negocio.astype('category')
+
+    data.barrio = data.barrio.str.lower().str.replace('_','-').str.replace("-", " ")
+
+    data.comuna_ciudadano = data.comuna_ciudadano.astype('Int64')
+    data.comuna_ciudadano = data.comuna_ciudadano.astype('category')
+
+    data["fecha_de_beneficio"] = pd.to_datetime(data["fecha_de_beneficio"], format='mixed', dayfirst=True)
+
+    data.monto_del_credito = data.monto_del_credito.str.replace('$','').str.replace(',','')
+    data.monto_del_credito = data.monto_del_credito.astype(float)
+
+    data.línea_credito = data.línea_credito.str.lower().str.strip('_').str.strip('-').str.strip().str.replace('_',' ').str.replace('-',' ')
+
     data.dropna(inplace = True)
     data.drop_duplicates(inplace = True)
 
-    return df
+    return data
